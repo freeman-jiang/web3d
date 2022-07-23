58ff632f94b370f38710f56a547f337fc848d380
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-07-23 14:39:34 +0200

    zk local proof
    Co-authored-by: Sujant Kumar Krishnavanshi <kumarskv.201cv253@nitk.edu.in>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.experiments = { asyncWebAssembly: true };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,45 @@ import {
 import type { NextPage } from "next";
 import { CNImage, CNLink } from "@/components/utility";
 import Example from "@/images/example.png";
+import { initialize } from "zokrates-js";
 
 const Home: NextPage = () => {
   const { toggleColorMode } = useColorMode();
+  initialize().then((zokratesProvider) => {
+    const source = `
+          def main(private field[3] inner_edge_lengths, field[3] outer_edge_lengths) {
+          assert(inner_edge_lengths[0] < outer_edge_lengths[0]);
+          assert(inner_edge_lengths[1] < outer_edge_lengths[1]);
+          assert(inner_edge_lengths[2] < outer_edge_lengths[2]);
+          return;
+        }`;
+
+    const artifacts = zokratesProvider.compile(source);
+
+    // computation
+    const { witness, output } = zokratesProvider.computeWitness(artifacts, [
+      ["10", "10", "10"],
+      ["100", "100", "100"],
+    ]);
+
+    // run setup
+    const keypair = zokratesProvider.setup(artifacts.program);
+
+    // generate proof
+    const proof = zokratesProvider.generateProof(
+      artifacts.program,
+      witness,
+      keypair.pk
+    );
+
+    // export solidity verifier
+    const verifier = zokratesProvider.exportSolidityVerifier(keypair.vk);
+
+    // or verify off-chain
+    const isVerified = zokratesProvider.verify(keypair.vk, proof);
+
+    console.log("verirfied", isVerified);
+  });
   return (
     <Flex
       direction="column"
